f32f24bb9b5a2b1b89a372ca340d2ac269caa1e2
Bach Ngoc Toan <ngoctoan@live.com>
2020-09-11 21:11:13 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -1,54 +1,50 @@
-import React, { useState } from "react";
-import "./App.css";
-import Person from "./components/Person/Person";
-import Example from "./components/Example";
+import React, { useState } from 'react';
+import './App.css';
+import Person from './components/Person/Person';
+import Example from './components/Example';
 
 function App() {
+  const [showPerson, setShowPerson] = useState(true);
   const [persons, setPersons] = useState([
-    { name: "TEDU 1", age: 5 },
-    { name: "TEDU 2", age: 6 },
-    { name: "TEDU 3", age: 7 },
+    { name: 'TEDU 1', age: 5 },
+    { name: 'TEDU 2', age: 6 },
+    { name: 'TEDU 3', age: 7 },
   ]);
-  const changeNameHandler = (e) => {
-    setPersons([
-      { name: "TEDU 4", age: 1 },
-      { name: "TEDU 5", age: 2 },
-      { name: "TEDU 6", age: 3 },
-    ]);
+
+  const deletePerson = (personIndex)=>{
+    console.log(personIndex);
+    let newPersons = [...persons];
+    newPersons.splice(personIndex,1);
+    setPersons(newPersons);
+  }
+  const togglePersons = () => {
+    setShowPerson(!showPerson);
   };
 
-  const switchNameHandler = (event) => {
-    setPersons([
-      { name: event.target.value, age: 1 },
-      { name: "TEDU 5", age: 2 },
-      { name: "TEDU 6", age: 3 },
-    ]);
-  };
+  let personList = null;
+
+  if (showPerson === true) {
+    personList = (
+      <div>
+        {persons.map((item,index) => {
+          return (
+            <Person key={index}
+              click={() => deletePerson(index)}
+              name={item.name}
+              age={item.age}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+
   return (
     <>
       <div className="App">
         <h1>This is the first ReactJS app</h1>
-        <button onClick={() => changeNameHandler()}>Change name</button>
-        <Person
-          click={() => changeNameHandler()}
-          changed={switchNameHandler}
-          name={persons[0].name}
-          age={persons[0].age}
-        />
-        <Person
-          click={changeNameHandler}
-          changed={switchNameHandler}
-          name={persons[1].name}
-          age={persons[1].age}
-        />
-        <Person
-          changed={switchNameHandler}
-          click={changeNameHandler}
-          name={persons[2].name}
-          age={persons[2].age}
-        />
-
-        <Example></Example>
+        <button onClick={() => togglePersons()}>Toggle Persons</button>
+        {personList}
       </div>
     </>
     // React.createElement('div',{className:'App'}, React.createElement('h1',null, 'This is the first ReactJS app'))
