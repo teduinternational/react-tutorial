ee028ebe5ac6c7018b641a7669a949680bff2ec3
Toan Bach <tedu.international@gmail.com>
2020-10-24 15:04:31 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React, { useState } from 'react';
 import './App.css';
+
+import React, { useState } from 'react';
+
 import Person from './components/Person/Person';
-import Example from './components/Example';
 
 function App() {
   const [showPerson, setShowPerson] = useState(true);
   const [persons, setPersons] = useState([
-    { name: 'TEDU 1', age: 5 },
-    { name: 'TEDU 2', age: 6 },
-    { name: 'TEDU 3', age: 7 },
+    { id: 1, name: 'TEDU 1', age: 5 },
+    { id: 2, name: 'TEDU 2', age: 6 },
+    { id: 3, name: 'TEDU 3', age: 7 },
   ]);
 
-  const deletePerson = (personIndex)=>{
+  const deletePerson = (personIndex) => {
     console.log(personIndex);
     let newPersons = [...persons];
-    newPersons.splice(personIndex,1);
+    newPersons.splice(personIndex, 1);
     setPersons(newPersons);
-  }
+  };
   const togglePersons = () => {
     setShowPerson(!showPerson);
+  };
+
+  const handleNameChanged = (event, id) => {
+    const personIndex = persons.findIndex((p) => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...persons[personIndex],
+    };
+
+    person.name = event.target.value;
+
+    const newPersons = [...persons];
+
+    newPersons[personIndex] = person;
+
+    setPersons(newPersons);
   };
 
   let personList = null;
@@ -26,12 +45,16 @@ function App() {
   if (showPerson === true) {
     personList = (
       <div>
-        {persons.map((item,index) => {
+        {persons.map((item, index) => {
           return (
-            <Person key={index}
+            <Person
+              key={index}
               click={() => deletePerson(index)}
               name={item.name}
               age={item.age}
+              changed={(event) => {
+                handleNameChanged(event, item.id);
+              }}
             />
           );
         })}
@@ -41,7 +64,7 @@ function App() {
 
   return (
     <>
-      <div className="App">
+      <div className='App'>
         <h1>This is the first ReactJS app</h1>
         <button onClick={() => togglePersons()}>Toggle Persons</button>
         {personList}
